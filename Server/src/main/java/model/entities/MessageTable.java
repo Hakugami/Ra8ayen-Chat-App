26c21a346ceec5cb45fcf29c057f884512d88435
Rashida Moahmed <RAMO.21122019@gmail.com>
2024-01-05 23:47:46 +0200

    Message DAO Done

--- Server/src/main/java/model/entities/MessageTable.java
+++ Server/src/main/java/model/entities/MessageTable.java
@@ -1,2 +1,21 @@
-package model.entities;public class MessageTable {
+package model.entities;
+
+public enum MessageTable {
+    MessageID("MessageID"),
+    SenderID("SenderID"),
+    ReceiverID("ReceiverID"),
+    MessageContent("MessageContent"),
+    MessageTimestamp("MessageTimestamp"),
+    MessageFontStyle("FontStyle"),
+    MessageFontColor("FontColor"),
+    TextBackground("TextBackground"),
+    FontSize("FontSize"),
+    Bold("Bold"),
+    Italic("Italic"),
+    Underline("Underline"),
+    Emoji("Emoji");
+    public String name;
+    MessageTable(String name){
+        this.name= name;
+    }
 }
