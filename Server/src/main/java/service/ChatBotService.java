a50c51266dcfcd08db85a2299392b0ce85486c4c
MostafaAbdallah99 <mostafaabdallah009@gmail.com>
2024-02-10 20:02:32 +0200

    fix: Fixed chat bot to sleep for 5 seconds before responding in case two chat bots talk to each other

--- Server/src/main/java/service/ChatBotService.java
+++ Server/src/main/java/service/ChatBotService.java
@@ -1,11 +1,14 @@
 package service;
 
 import com.google.code.chatterbotapi.*;
+
 public class ChatBotService {
     public String getResponse(String message) throws Exception {
         ChatterBotFactory factory = new ChatterBotFactory();
         ChatterBot pandoraBot = factory.create(ChatterBotType.PANDORABOTS, "b0dafd24ee35a477");
         ChatterBotSession pandoraBotSession = pandoraBot.createSession();
-        return pandoraBotSession.think(message);
+        String response = pandoraBotSession.think(message);
+        Thread.sleep(5000);
+        return response;
     }
 }
